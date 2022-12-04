fecf1f6827a2d6fb675bc2ed4b95c7e9e62588b8
piyawatklab <bobbybansong@gmail.com>
2022-12-04 22:33:05 +0700

    เพิ่ม html

--- main.py
+++ main.py
@@ -8,26 +8,21 @@ import models
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/test")
-def root():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+@app.get("/test/", response_class=HTMLResponse)
+def index(request: Request):
+    context = {'request': request, 'data': 75}
+    return templates.TemplateResponse("item.html", context)
+
 
 @app.get("/")
 async def root():
     return {"message": "test"}
+
 
 @app.get("/all")
 def get_all():
@@ -35,13 +30,21 @@ def get_all():
     return data
 
 
-@app.get("/get_current_price")
+@app.get("/get_current_price_api")
 def get_one(date: str):
     data = db.get_one(date)
     return data
-# def get_last():
-#     data = db.get_last()
-#     return {"data": data}
+
+
+@app.get("/get_current_price", response_class=HTMLResponse)
+def get_one(request: Request, date="", price_type="ask"):
+    data = db.get_one(date)
+    context = {'request': request,
+               'date': data["date"],
+               'price': data["price_"+price_type],
+               'price_type': price_type}
+
+    return templates.TemplateResponse("item.html", context)
 
 
 @app.get("/get_all_by_date/{date}")
